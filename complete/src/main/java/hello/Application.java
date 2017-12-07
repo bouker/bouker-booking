d4d2513df45ffc559854b4538ad47130863ae400
KamilBugno <bugno@student.agh.edu.pl>
2017-12-07 16:19:55 +0100

    send post for event

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,13 +2,16 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 
 @SpringBootApplication
 @RestController
 public class Application {
+    private String REST_SERVICE_URI = "http://192.168.99.100:8080/";
 
     @RequestMapping("/")
     public String home() {return "complete Hello Docker World";}
@@ -18,9 +21,22 @@ public class Application {
 
         // TODO
         //save input into database
-        //decrease number of people
-        //send UI confirmation
-        return input.email;
+        String response = sendPost(input);
+        //return UI confirmation
+        return "add " + input.email+ ", response: " + response;
+    }
+
+    public String sendPost(Reservation reservation){
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.postForObject(REST_SERVICE_URI+"/event/"
+                + reservation.eventId, reservation, String.class);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/event/{event_id}")
+    public String event(@RequestBody Reservation input, @PathVariable Long event_id) {
+
+        return "event " + event_id + " " + input.email;
     }
 
     public static void main(String[] args) {
