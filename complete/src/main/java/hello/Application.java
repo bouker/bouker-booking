ef332be1fbf6466a56b77bb197118447649a8ba4
KamilBugno <bugno@student.agh.edu.pl>
2017-12-11 13:42:34 +0100

    change /event to /events

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,35 +1,25 @@
 package hello;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 @SpringBootApplication
 @RestController
 public class Application {
-    private String REST_SERVICE_URI = "http://localhost:8080/";//"http://192.168.99.100:8080/";
+    private String REST_SERVICE_URI = "http://192.168.99.100:8080/";//"http://localhost:8080/";
 
     @Autowired
     ReservationRepository repository;
@@ -60,11 +50,11 @@ public class Application {
     public int sendPostToEvent(Reservation reservation) throws IOException {
         RestTemplate restTemplate = new RestTemplate();
 //
-//        return restTemplate.postForObject(REST_SERVICE_URI+"/event/"
+//        return restTemplate.postForObject(REST_SERVICE_URI+"/events/"
 //                + reservation.getEventId(), new NumberOfContestants(Integer.parseInt(reservation.getNumber())), HttpStatus.class);
 
         CloseableHttpClient client = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(REST_SERVICE_URI+"/event/" + reservation.getEventId());
+        HttpPost httpPost = new HttpPost(REST_SERVICE_URI+"/events/" + reservation.getEventId());
 
         String json = "{\"number\":\"" +reservation.getNumber() + "\"}";
         StringEntity entity = new StringEntity(json);
@@ -77,7 +67,7 @@ public class Application {
         return response.getStatusLine().getStatusCode();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/event/{event_id}")
+    @RequestMapping(method = RequestMethod.POST, value = "/events/{event_id}")
     public int event(@RequestBody NumberOfContestants input, @PathVariable Long event_id) {
         //...
         return HttpStatus.FOUND.value();
